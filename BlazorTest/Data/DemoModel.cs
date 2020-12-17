8abb5ded0ad4055161f2ba4f988ebdfded0d7ccb
Ace Alrich <hex0@live.com>
2020-12-17 22:13:36 +0000

    Add project files.

---
+++ BlazorTest/Data/DemoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Data
+{
+    public class DemoModel
+    {
+        public int age { get; set; }
+        public CarType TypeOfCar { get; set; }
+    }
+}
---
+++ BlazorTest/Data/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Data
+{
+    public enum CarType
+    {
+        Tesla,
+        BMW,
+        Ford,
+        Mercedes
+    }
+}
